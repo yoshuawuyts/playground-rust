c078ce06ebce49b278af739851607eda79e7cbe0
Yoshua Wuyts <i@yoshuawuyts.com>
2015-09-29 02:17:59 +1000

    create: add bundle.sh template

--- create.sh
+++ create.sh
@@ -6,6 +6,11 @@ count=$(printf "%02d" $count)
 name="$count"_"$1"
 
 mkdir "$name"
+
 touch "$name"/index.rs
 touch "$name"/bundle.sh
 chmod +x "$name"/bundle.sh
+cat > "$name"/bundle.sh << EOF
+#!/bin/sh
+rustc ./index.rs -o bundle
+EOF
