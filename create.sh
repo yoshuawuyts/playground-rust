4b51a9c08be418984dc266b8df28acc1311ac904
Yoshua Wuyts <i@yoshuawuyts.com>
2015-10-01 09:30:43 +1000

    create: use cargo

--- create.sh
+++ create.sh
@@ -5,12 +5,4 @@ count=$((count - 5))
 count=$(printf "%02d" $count)
 name="$count"_"$1"
 
-mkdir "$name"
-
-touch "$name"/main.rs
-touch "$name"/bundle.sh
-chmod +x "$name"/bundle.sh
-cat > "$name"/bundle.sh << EOF
-#!/bin/sh
-rustc ./main.rs -o bundle
-EOF
+cargo new "$name" --bin
